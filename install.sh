d2c4ccd8eeac203a9d6cc7aee9a6bc699a338825
gq.guo <gq.guo@gqguo-Mac.local>
2020-07-10 09:43:20 +0800

    update

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ cd /Users/gq.guo/.jenkins/workspace/api-test-run
 source /Users/gq.guo/tmp/api_test/apitest-venv/bin/activate
 pip uninstall aftership-api-test
 pip install aftership*.whl
+httprunner startproject api-test
